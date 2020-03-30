76e44e015aa2ba3b8b2ed97d5c2b0403a9982fd9
flycun <flycun@163.com>
2020-03-30 16:59:52 +0800

    init java design patterns

---
+++ java-design-patterns/src/test/java/com/flycun/patterns/JavaDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flycun.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
